3ca9b617dd327aef9967505af8b087fc17d95fe5
mohammed hajoubi <hajomodo@gmail.com>
2023-10-06 16:59:01 -0700

    putchar printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'a program that prints alphabets lower case using putchar'
+ *Return: value 0 no problem
+*/
+int main(void)
+{
+   for (char c = 'a'; c <= 'z'; c++)
+   {
+        putchar(c);
+   }
+    return (0);
+}
