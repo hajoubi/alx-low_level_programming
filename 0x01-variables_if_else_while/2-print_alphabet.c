45b9d9048b2dced287b9dae4a40c684eabcbb07a
mohammed hajoubi <hajomodo@gmail.com>
2023-10-06 17:08:04 -0700

    corect format

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,12 @@
 */
 int main(void)
 {
-   for (char c = 'a'; c <= 'z'; c++)
-   {
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
         putchar(c);
-   }
+    }
+    putchar('\n');
     return (0);
 }
