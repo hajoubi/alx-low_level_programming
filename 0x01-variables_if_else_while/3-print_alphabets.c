f240f61c6b42eb93f2a1cbe08c3103015b33913d
mohammed hajoubi <hajomodo@gmail.com>
2023-10-06 17:14:56 -0700

    lower and upercase alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'a program that prints alphabets lowercase and uppercase using putchar'
+ *Return: value 0 no problem
+*/
+int main(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        putchar(c);
+    }
+    for (c = 'A'; c <= 'Z'; c++)
+    {
+        putchar(c);
+    }
+    putchar('\n');
+    return (0);
+}
