99e72375f5a8a1c50c82f46ee4a1042c3e56a3e1
mohammed hajoubi <hajomodo@gmail.com>
2023-10-16 16:24:22 -0700

    correcting

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,18 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _puts - print a string to stdout
- * @str: char array string type
- * Description: Can only use _putchar
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-void _puts(char *str)
+int _putchar(char c)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+	return (write(1, &c, 1));
 }
