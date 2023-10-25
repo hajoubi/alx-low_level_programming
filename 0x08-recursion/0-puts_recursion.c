9215569d43338e4ef3fad996c51acf50f298667d
mohammed hajoubi <hajomodo@gmail.com>
2023-10-25 16:30:28 -0700

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: Pointer
+ * @i: iter var
+ * Return: void.
+ */
+
+
+void _puts_recursion(char *s)
+{
+    int i;
+    i = 0;
+    if(s[i] != '\0' )
+    {
+        _putchar(s[i]);
+        i++;
+        _puts_recursion(s+i);
+    }
+    else 
+    {
+        _putchar('\n');
+		return;
+    } 
+
+}
