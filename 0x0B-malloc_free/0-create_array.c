3fd46d78bf3c91013ba05165deb9ddd5d9614f28
mohammed hajoubi <hajomodo@gmail.com>
2023-10-30 15:45:19 -0700

    handeling errors

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *ptr;
-    ptr = (char *)malloc(size * sizeof(char));
-    for (i = 0 ; i < size ; i++)
-    {
-        ptr[i] = c;
-    }
+unsigned int i;
+char *ptr;
+ptr = (char *)malloc(size * sizeof(char));
+if (size == 0)
+	return (NULL);
+if (!ptr)
+	return (NULL);
+for (i = 0 ; i < size ; i++)
+{
+    ptr[i] = c;
+}
 return (ptr);
 }
